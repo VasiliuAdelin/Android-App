707fc82614a49e66b8aca113147d9297a0f5c27d
Vasiliu Adelin <vasiliu.gheorghe.adelin@gmail.com>
2020-03-18 19:28:40 +0200

    Lab 2 commit

---
+++ app/src/main/java/com/example/petshop/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.petshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    TextView textView;
+    String[] listItem;
+    ArrayList<String> clickedItems = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onCreate invoked");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        textView = (TextView) findViewById(R.id.textView);
+
+        listItem = getResources().getStringArray(R.array.array_technology);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String selectedItem = adapter.getItem(position);
+                clickedItems.add(selectedItem);
+                String aux = "";
+                for(String i:clickedItems){
+                    aux += i;
+                    aux+= '\n';
+                }
+                Toast.makeText(getApplicationContext(), aux, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle", "onStart invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle", "onResume invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle", "onPause invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle", "onStop invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle", "onRestart invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle", "onDestroy invoked");
+    }
+
+}
